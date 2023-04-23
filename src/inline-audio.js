72af5cfb8f12565065cd7fbd47667a9fa33bbede
Paul Simkonis <122229125+SpruceWoodTree@users.noreply.github.com>
2023-04-23 19:08:05 -0400

    added a toggle method, etc
    Added:
    - Method for toggling play/pause
    - The simple-icon button implementation for the play and pause button
    - audio tag to play the audio
    - some css parameters
    - some variables to help out the audio toggle

--- src/inline-audio.js
+++ src/inline-audio.js
@@ -1,10 +1,14 @@
 import { LitElement, html, css } from 'lit';
 import '@lrnwebcomponents/simple-icon/simple-icon.js';
 import '@lrnwebcomponents/simple-icon/lib/simple-icons.js';
+import '@lrnwebcomponents/simple-colors/simple-colors.js';
 
 class InlineAudio extends LitElement {
   static properties = {
     header: { type: String },
+    playStateIcon: { type: String },
+    audio: { type: String },
+    audioLoaded: { type: Boolean },
   }
 
   static styles = css`
@@ -18,14 +22,33 @@ class InlineAudio extends LitElement {
     .cardDesign{
       display:inline-flex;
       align-items: center;
-      padding: 4px 4px 4px 4px;
-      
+      padding: 4px 4px 4px 0px;
+      border: 2px solid black;
+      min-width: 50px;
+      font-size: 12px;
     }
   `;
 
   constructor() {
     super();
+    this.playStateIcon = "av:play-arrow";
+    this.audioLoaded = "false";
+  }
 
+  
+
+  audioToggle(){
+    if(document.shadowRoot.querySelector(".player").hasAttribute("src") != true){
+      const customAudio = document.shadowRoot.querySelector(".player");
+      customAudio.src = audio;
+    }
+    
+    if(document.shadowRoot.querySelector(".player").paused && this.audioLoaded == true){
+      
+    }
+    else if(document.shadowRoot.querySelector(".player").paused == false && this.audioLoaded == true){
+
+    }
   }
 
 
@@ -34,7 +57,9 @@ class InlineAudio extends LitElement {
     return html`
       <main>
         <div class="cardDesign">
-
+          <simple-icon icon="${this.playStateIcon}" @click="${document.audioToggle()}"></simple-icon>
+          <slot></slot>
+          <audio class="player" type="audio/mpeg"></audio>
         </div>
       </main>
     `;
